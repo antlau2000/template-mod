8882198b5fee8e5dbd6cf687c92200fc7b0b7952
antlau2000 <antlau2000@yandex.ru>
2025-10-26 15:35:35 +0300

    delete debugging code

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -4,12 +4,9 @@ import com.example.database.DatabaseManager;
 import com.example.network.MessagePayload;
 import com.example.network.NetworkHandler;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
 
 public class TemplateMod implements ModInitializer {
 	public static final String MOD_ID = "template-mod";
@@ -26,14 +23,5 @@ public class TemplateMod implements ModInitializer {
 		} catch (Exception e) {
 			LOGGER.error("Database initialization failed", e);
 		}
-
-		ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
-			TemplateMod.LOGGER.info(">>> SERVER STOPPING - triggered by: " +
-					Arrays.toString(Thread.currentThread().getStackTrace())); // Кто вызвал остановку
-		});
-
-		ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
-			TemplateMod.LOGGER.info(">>> SERVER STOPPED");
-		});
 	}
 }
